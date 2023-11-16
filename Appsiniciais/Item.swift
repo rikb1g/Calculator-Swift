a3156e3d360d4fd7932964d9f1639d2165204579
Ricardo Sousa <r.sousa.888@gmail.com>
2023-11-16 23:01:30 +0000

    Initial Commit

---
+++ Appsiniciais/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Appsiniciais
+//
+//  Created by Ricardo Sousa on 16/11/2023.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
